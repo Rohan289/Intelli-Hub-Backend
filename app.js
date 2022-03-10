8809693e11264c71d9ae60b876e00d23fb061ac3
Rohan289 <rbhowmick184@gmail.com>
2022-03-10 23:26:33 +0530

    Initialized branch

---
+++ app.js
@@ -0,0 +1,16 @@
+// app.js
+const express = require('express');
+const cors = require('cors');
+const {ticketDetails} = require("./covidData");
+const app = express();
+app.use(cors());
+app.get('/', (req, res) => res.send('Hello!'));
+app.get('/list_ticket_details',(req,res) => {
+    let response = [];
+    ticketDetails.map((element) => {
+        response.push(element);
+    });
+    res.status(200).send(response);
+})
+
+app.listen(8080, () => console.log('running on port 8080'));
---
+++ covidData.js
@@ -0,0 +1,31 @@
+const ticketDetails = [
+    {
+        name : "Need to change my shipping address",
+        type : "Story",
+        assignee : "Rohan Bhowmick",
+        priority : "P1",
+        description : "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+    },
+    {
+        name : "Need to change my delivery address",
+        type : "Bug",
+        assignee : "Rohan Bhowmick",
+        priority : "P2",
+        description : "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."
+    },
+    {
+        name : "Need to change my shipping unit",
+        type : "Epic",
+        assignee : "Prakhar Ranjan",
+        priority : "P3",
+        description : "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."   
+    },
+    {
+        name : "Need to change my delivery date",
+        type : "Task",
+        assignee : "Swarnim Bhatt",
+        priority : "P3",
+        description : "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like."   
+    }
+]
+exports.ticketDetails = ticketDetails;
